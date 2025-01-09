1f7036148c84186cad47db2d8907cd37c4db9a66
StackMous <tarvo.saarinen@gmail.com>
2025-01-09 13:08:22 +0200

    notes-backend, kayttajien hallinta, 2 testia ei toimi

--- osa4/notes-backend/controllers/notes.js
+++ osa4/notes-backend/controllers/notes.js
@@ -1,35 +1,11 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
-})
-
-notesRouter.get('/:id', async (request, response) => {
-  const note =await Note.findById(request.params.id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-notesRouter.post('/', async (request, response, next) => {
-  const body = request.body
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  const savedNote = await note.save()
-  response.status(201).json(savedNote)
-})
-
-notesRouter.delete('/:id', async (request, response) => {
-  await Note.findByIdAndDelete(request.params.id)
-  response.status(204).end()
 })
 
 notesRouter.put('/:id', async (request, response) => {
@@ -42,6 +18,47 @@ notesRouter.put('/:id', async (request, response) => {
 
   const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
   response.json(updatedNote)
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+  console.log('tarvo body')
+  console.log(body)
+
+  console.log(`tarvo userId ${body.userId}`)
+  const user = await User.findById(body.userId)
+  //const user = User.findById(body.userId)
+  console.log(`tarvo user ${user}`)
+
+  const note = new Note({
+    content: body.content,
+    //important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user._id
+  })
+  console.log(`tarvo new note `)
+  console.log(note)
+
+
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.status(201).json(savedNote)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+  const note =await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = notesRouter
--- osa4/notes-backend/mongo.js
+++ osa4/notes-backend/mongo.js
@@ -8,7 +8,9 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://StackMous:${password}@stackmouscluster.naieb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=StackMousCluster`
+  `mongodb+srv://StackMous:${password}@stackmouscluster.naieb.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=StackMousCluster`
+// or this? `mongodb+srv://StackMous:${password}@stackmouscluster.naieb.mongodb.net/noteApp?retryWrites=true&w=majority`
+//  `mongodb+srv://StackMous:${password}@stackmouscluster.naieb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=StackMousCluster`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,17 +21,16 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  /*
   const note = new Note({
-    content: 'HTML is x',
+    content: 'Eisssaaatana',
     important: true,
   })
 
-    note.save().then(result => {
-      console.log('note saved!')
-      mongoose.connection.close()
-    })
-    */
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+
   Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
